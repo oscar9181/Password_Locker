23be01323c80c1ecae93aaa0c919e6528ee7f687
oscar9181 <oscarokola17@gmail.com>
2022-04-25 15:32:44 +0300

    save user

--- user_test.py
+++ user_test.py
@@ -21,7 +21,17 @@ def test__init(self):
     self.assertEqual(self.new__user.user_name,"Oscarero")
     self.assertEqual(self.new__user.pass_word,"oscar9181")
 
+
+    def test_save_user(self):
+        '''
+        test_save_contact test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+
     
-  
+
 
 
